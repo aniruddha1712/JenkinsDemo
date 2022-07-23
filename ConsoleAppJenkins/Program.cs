1c18128e51e4060648d35b9eb0dc337c96ad4dd6
Aniruddha <aniruddha1712@gmail.com>
2022-07-23 17:17:48 +0530

    [Aniruddha]Added: 2 methods and 2 test cases

--- ConsoleAppJenkins/Program.cs
+++ ConsoleAppJenkins/Program.cs
@@ -8,5 +8,13 @@ namespace ConsoleAppJenkins
         {
             Console.WriteLine("Hello World!");
         }
+        public static void Method1()
+        {
+            Console.WriteLine("Hello World!!");
+        }
+        public static void Method2()
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
 }
--- TestProjectJen/UnitTest1.cs
+++ TestProjectJen/UnitTest1.cs
@@ -7,17 +7,30 @@ namespace TestProjectJen
     [TestClass]
     public class UnitTest1
     {
-        private const string Expected = "Hello World!";
+        private const string Expected1 = "Hello World!!";
+        private const string Expected2 = "Hello World!";
         [TestMethod]
         public void TestMethod1()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                ConsoleAppJenkins.Program.Main();
+                ConsoleAppJenkins.Program.Method1();
 
                 var result = sw.ToString().Trim();
-                Assert.AreEqual(Expected, result);
+                Assert.AreEqual(Expected1, result);
+            }
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleAppJenkins.Program.Method1();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected2, result);
             }
         }
     }
